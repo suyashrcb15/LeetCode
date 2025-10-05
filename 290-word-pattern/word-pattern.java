d068927f442fee113b97f4623bc704cfb86258b9
Suyash Tiwari <142603779+suyashrcb15@users.noreply.github.com>
2025-10-05 12:06:54 +0530

    Time: 1 ms (79.83%) | Memory: 41.7 MB (18.87%) - LeetSync

---
+++ 290-word-pattern/word-pattern.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        String[] words = s.split(" ");
+        if (pattern.length() != words.length) return false;
+
+        Map<Character, String> map = new HashMap<>();
+        Set<String> used = new HashSet<>();
+
+        for (int i = 0; i < pattern.length(); i++) {
+            char c = pattern.charAt(i);
+            String word = words[i];
+
+            if (map.containsKey(c)) {
+                if (!map.get(c).equals(word)) return false;
+            } else {
+                if (used.contains(word)) return false;
+                map.put(c, word);
+                used.add(word);
+            }
+        }
+        return true;
+    }
+}
